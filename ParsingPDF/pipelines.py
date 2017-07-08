b614719af6bab762f3627b3b1b6edf553e9ac788
realsoftdev <devfinal310@gmail.com>
2017-07-08 08:52:10 -0400

    modify pipelines.py

--- ParsingPDF/pipelines.py
+++ ParsingPDF/pipelines.py
@@ -5,8 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-from scrapy.pipelines.files import FilePipelines
+from scrapy.pipelines.files import FilesPipeline
 
-class ParsingpdfPipeline(object):
-    def process_item(self, item, spider):
+class ParsingpdfPipeline(FilesPipeline):
+
+    def get_media_requests(self, item, info):
+        meta = {
+            'filename': item['company_name']
+        }
+        for image_url in item['file_urls']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem("Item contains no images")
+        item['image_paths'] = image_paths
         return item
+
+    def file_path(self, request, response=None, info=None):
+        filename = request.meta.get('filename', '')
+        return '%s.%s' % (filename, 'pdf')
+    # def file_path(self):
